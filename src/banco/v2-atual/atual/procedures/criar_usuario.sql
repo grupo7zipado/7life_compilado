9866bde7550a780f5d783e9856cac7a6362c81a9
gabriell122 <gabriell.haines@etec.sp.gov.br>
2026-04-04 23:58:44 -0300

    v1.0.9.1 - Melhorias procedore cadastro usuários

--- src/banco/v2-atual/atual/procedures/criar_usuario.sql
+++ src/banco/v2-atual/atual/procedures/criar_usuario.sql
@@ -9,47 +9,64 @@ CREATE PROCEDURE proc_criar_usuario (
     IN p_nascimento DATE,
     IN p_tipo ENUM("admin", "user")
 )
-BEGIN 
-	DECLARE v_usu_id INT;
+main:BEGIN 
+	DECLARE _usu_id INT;
+    DECLARE _sucess BOOLEAN DEFAULT TRUE;
+    DECLARE _message VARCHAR(255) DEFAULT '';
     IF 
         p_nome IS NULL 
      OR p_email IS NULL 
      OR p_senha IS NULL 
      OR p_nascimento IS NULL
     THEN
-        SIGNAL SQLSTATE '45000' 
-        SET MESSAGE_TEXT = 'Todos os campos são obrigatórios';
+        SELECT 
+            0 AS _sucess,
+            'Dados Invalidos' AS _message;
+        LEAVE main;
     END IF;
+	
+    IF EXISTS (
+        SELECT 1 
+        FROM usuarios 
+        WHERE usu_email = p_email
+    ) THEN
 
-    START TRANSACTION;
+        -- já existe
+        SELECT 
+            0 AS _sucess,
+            'Email já cadastrado' AS _message;
+        LEAVE main;
+    ELSE
+    
+        START TRANSACTION;
 
-    INSERT INTO usuarios (
-        usu_nome
-       ,usu_nascimento
-    )
-    VALUES (
-        p_nome
-       ,p_nascimento
-    );
+        INSERT INTO usuarios (
+            usu_nome
+        ,usu_nascimento
+        )
+        VALUES (
+            p_nome
+        ,p_nascimento
+        );
 
-    SET v_usu_id = LAST_INSERT_ID();
+        SET _usu_id = LAST_INSERT_ID();
 
-    INSERT INTO usuariosA (
-        usu_id
-       ,usu_email
-       ,usu_password
-       ,usu_tipo
-    )
-    VALUES (
-        v_usu_id
-       ,p_email
-       ,p_senha
-       ,COALESCE(p_tipo, 'user')
-    );
-
-    COMMIT;
-
-
+        INSERT INTO usuariosA (
+            usu_id
+        ,usu_email
+        ,usu_password
+        ,usu_tipo
+        )
+        VALUES (
+            _usu_id
+        ,p_email
+        ,p_senha
+        ,COALESCE(p_tipo, 'user')
+        );
+		SELECT _usu_id AS usu_id, 1 AS _sucess, 'Usuário criado com sucesso' AS _message;
+        COMMIT;
+	
+    END IF;
 
 END $$
 DELIMITER ;
