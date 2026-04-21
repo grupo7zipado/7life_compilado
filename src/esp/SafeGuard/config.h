018cd506ab3e060f370df624b113c8e1895caeb7
Midiel <midielcastroramires400@gmail.com>
2026-04-21 15:00:14 -0300

    Enhance config.h with WiFi configuration
    Added WiFi SSID and password definitions to config.h

--- src/esp/SafeGuard/config.h
+++ src/esp/SafeGuard/config.h
@@ -2,14 +2,18 @@
 #define CONFIG_H
 
 // OLED
-#define SCREEN_WIDTH 128
-#define SCREEN_HEIGHT 64
-#define OLED_RESET -1
+#define SCREEN_WIDTH 128     // Largura do display
+#define SCREEN_HEIGHT 64     // Altura do display
+#define OLED_RESET -1        // Pino de reset do display (-1 significa que o display não usa pino físico de reset)
 
 // Pinos
-#define SDA_PIN 8
-#define SCL_PIN 9
+#define SDA_PIN 8            // SDA I2C
+#define SCL_PIN 9            // SCL I2C
 
-#define BUZZER_PIN 10
+#define BUZZER_PIN 10        // Piezo
+
+// WiFi
+#define WIFI_SSID "Rede"     // Nome da rede
+#define WIFI_PASS "Senha"    // Senha da rede
 
 #endif
