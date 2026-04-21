83b0c68bf96ae408d8588f6cc3f43f1e4fd767e0
Midiel <midielcastroramires400@gmail.com>
2026-04-21 14:58:07 -0300

    Enhance setup and loop for WiFi and sensor management
    Refactor setup and loop functions to include WiFi maintenance and improve timing for sensor readings.

--- src/esp/SafeGuard/SafeGuard.ino
+++ src/esp/SafeGuard/SafeGuard.ino
@@ -1,6 +1,7 @@
 #include "config.h"
 #include "display.h"
 #include "sensors.h"
+#include "connections.h"
 
 #include <Wire.h>
 
@@ -8,16 +9,38 @@
 void setup() {
 
   Wire.begin(SDA_PIN, SCL_PIN);
-  initSensors();
-  initDisplay();
 
-  showBootScreen();
+  initDisplay();
+    showBootScreen();
+
+  initSensors();
+  initWiFi();
+
 }
 
+static unsigned long lastHeart = 0;
+static unsigned long lastTemp = 0;
+static unsigned long lastDisplay = 0;
+
 void loop() {
- readTemperature();
- readSpo2Bpm();
 
-  showMainScreen(abpm, spo2, temp);
+  maintainWiFi();
 
+  if (millis() - lastHeart > 20) {
+    lastHeart = millis();
+
+    readSpo2Bpm();
+  }
+
+  if (millis() - lastTemp > 1000) {
+    lastTemp = millis();
+
+   readTemperature();
+  }
+
+  if (millis() - lastDisplay > 500) {
+    lastDisplay = millis();
+
+    showMainScreen(abpm, spo2, temp);
+  }
 }
